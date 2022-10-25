b04a16c2206924443febb33f2a8658501becd163
mimi30422 <htm.hp304@gmail.com>
2022-10-25 19:09:46 +0700

    26

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure Node 24' });
+  res.render('index', { title: 'Azure Node 26' });
 });
 
 module.exports = router;
