0c01658342128b6f0028823dc6bbfcd4ab036987
mimi30422 <htm.hp304@gmail.com>
2022-10-25 14:09:21 +0700

    test 5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure Node 7' });
+  res.render('index', { title: 'Azure Node 8' });
 });
 
 module.exports = router;
